bf725cc9eea7e702cf8e18ea463d3633aad3369f
Suryansh Sharma <xazod444@gmail.com>
2026-08-01 11:32:05 +0530

    Update scan.py

---
+++ backend/app/api/routes/scan.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException
+
+from app.schemas.scan_schema import ScanRequest
+from app.services.nmap_service import NmapScanner
+from app.utils.validator import is_valid_target
+
+router = APIRouter()
+
+
+@router.post("/scan")
+def scan_target(request: ScanRequest):
+
+    if not is_valid_target(request.target):
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid target"
+        )
+
+    result = NmapScanner.quick_scan(request.target)
+
+    return result
